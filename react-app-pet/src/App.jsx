5f365d8d7900c6a388f4ff1761fd5230a067424d
GuilhermeMenezesdaSilva <rm89384@fiap.com.br>
2022-09-30 09:47:17 -0300

    Componente App criado

---
+++ react-app-pet/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PetCadastro from './components/PetCadastro'
+
+export default function App() {
+  return (
+    <div>
+      <h1>OPA CAPUTO</h1>
+      <PetCadastro/>
+    </div>
+  )
+}
