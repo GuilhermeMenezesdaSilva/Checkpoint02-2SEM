87b4f8776ddca1e81b71ec8fe1d26e3f651e1328
Luis <rm93025@fiap.com.br>
2022-10-02 22:07:37 -0300

    TXT e DOCX adicionado e consertado alguns erros.

--- react-app-pet/src/App.jsx
+++ react-app-pet/src/App.jsx
@@ -4,7 +4,7 @@ import PetCadastro from './components/PetCadastro'
 export default function App() {
   return (
     <div>
-      <h1>OPA CAPUTO</h1>
+      <h1></h1>
       <PetCadastro/>
     </div>
   )
